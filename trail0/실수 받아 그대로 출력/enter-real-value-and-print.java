f65a46b77b7523a11128c24313c2dbd3566bcffe
thqordl <147072011+thqordl@users.noreply.github.com>
2026-05-22 15:44:29 +0900

    [Passed] 실수 받아 그대로 출력 (158ms, 11MB)

---
+++ trail0/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float n = sc.nextFloat();
+
+        System.out.print(String.format("%.2f", n));
+    }
+}
