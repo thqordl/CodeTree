176f55cf009ce49306024090e7ea6b982e264de8
thqordl <147072011+thqordl@users.noreply.github.com>
2026-05-22 15:28:16 +0900

    [Passed] 점수 입출력 (120ms, 11MB)

---
+++ trail0/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String score = sc.next();
+        System.out.print("Your score is "+ score +" point.");
+    }
+}
