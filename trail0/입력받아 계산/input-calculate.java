6d4d66911cbefad84d48dc4d259b09c70910869b
thqordl <147072011+thqordl@users.noreply.github.com>
2026-05-22 15:40:51 +0900

    [Passed] 입력받아 계산 (134ms, 10MB)

---
+++ trail0/입력받아 계산/input-calculate.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = Integer.parseInt(sc.next());
+
+        System.out.print(a+2);
+    }
+}
