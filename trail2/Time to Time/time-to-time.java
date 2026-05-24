d6e217d45bebbaedd66e447914aca5988a611ae4
thqordl <147072011+thqordl@users.noreply.github.com>
2026-05-24 17:26:44 +0900

    [Passed] Time to Time (127ms, 10MB)

--- trail2/Time to Time/time-to-time.java
+++ trail2/Time to Time/time-to-time.java
@@ -6,37 +6,10 @@ public class Main {
         int b = sc.nextInt();
         int c = sc.nextInt();
         int d = sc.nextInt();
-        
-        int ab = 0;
-        int cd = 0;
-        int hour = 0;
-        int min = 0;
-        
-        while (true) {
-            if(hour==a && min==b) {
-                break;
-            }
-            ab++;
-            min++;
-            if(min==60) {
-                hour++;
-                min=0;
-            }
-        }
+                     
+        int ab = 60*a + b;
+        int cd = 60*c + d;
 
-        hour = 0;
-        min = 0;
-        while (true) {
-            if(hour==c && min==d) {
-                break;
-            }
-            cd++;
-            min++;
-            if(min==60) {
-                hour++;
-                min=0;
-            }
-        }
         System.out.print(cd-ab);
     }
 }
