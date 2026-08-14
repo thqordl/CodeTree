2247a090e270f9ad581fc4790f0824825261e9e3
thqordl <147072011+thqordl@users.noreply.github.com>
2026-08-14 11:21:44 +0900

    [Passed] 남녀노소 구분짓기 (57ms, 16MB)

---
+++ trail1/남녀노소 구분짓기/sex-and-age.py
@@ -0,0 +1,12 @@
+gen = int(input())
+age = int(input())
+if gen==0:
+    if age>=19:
+        print('MAN')
+    else:
+        print('BOY')
+else:
+    if age>=19:
+        print('WOMAN')
+    else:
+        print('GIRL')
