159becb8e2913423cad96b83c46789f554d692b6
thqordl <147072011+thqordl@users.noreply.github.com>
2026-08-18 13:27:58 +0900

    [Passed] 5의 배수의 합 (51ms, 16MB)

---
+++ trail1/5의 배수의 합/sum-of-specific-condition-multiple.py
@@ -0,0 +1,12 @@
+a, b = map(int,input().split())
+sum=0
+if b>=a:
+    for i in range(a, b+1):
+        if i%5==0:
+            sum+=i
+    print(sum)
+else:
+    for i in range(b, a+1):
+        if i%5==0:
+            sum+=i
+    print(sum)
