2d71ea1c9286d79002b1cd8ad6c81c0a39bbef8a
thqordl <147072011+thqordl@users.noreply.github.com>
2026-08-18 15:00:11 +0900

    [Passed] 출석 부르기 2 (48ms, 16MB)

---
+++ trail1/출석 부르기 2/calling-attendance-2.py
@@ -0,0 +1,13 @@
+while True:
+    no = int(input())
+    if no==1:
+        print('John')
+    elif no==2:
+        print('Tom')
+    elif no==3:
+        print('Paul')
+    elif no==4:
+        print('Sam')
+    else:
+        print('Vacancy')
+        break
