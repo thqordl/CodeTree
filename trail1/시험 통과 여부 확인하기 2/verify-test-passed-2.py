bc49d3d4b6b4971c743c2cc55b7e92aba636f9c4
thqordl <147072011+thqordl@users.noreply.github.com>
2026-08-20 14:50:57 +0900

    [Passed] 시험 통과 여부 확인하기 2 (49ms, 16MB)

---
+++ trail1/시험 통과 여부 확인하기 2/verify-test-passed-2.py
@@ -0,0 +1,11 @@
+n=int(input())
+cnt=0
+for i in range(n):
+    score=list(map(int,input().split()))
+    avg=sum(score)/len(score)
+    if avg>=60:
+        print('pass')
+        cnt+=1
+    else:
+        print('fail')
+print(cnt)
